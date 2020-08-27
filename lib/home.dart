175469776981bc6eb14153728190710c0beb3a62
yusril <projectyusril@gmail.com>
2020-08-27 10:45:46 +0700

    - rename

--- lib/home.dart
+++ lib/home.dart
@@ -40,7 +40,7 @@ class _HomeState extends State<Home> {
                               style: TextStyle(fontSize: 22),
                             ),
                             Text(
-                              "Dreamwalker",
+                              "Yusril",
                               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                             )
                           ],
